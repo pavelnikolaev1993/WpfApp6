5e1e503e8990187f4043fe2aa2b2acc3c103693b
Павел Николаев <pavelnikolaev1993@mail.ru>
2022-04-10 12:49:41 +0300

    Добавьте файлы проекта.

---
+++ WeatherConrtol.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp6
+{
+     class WeatherConrtol:DependencyObject
+    {
+        private string wind_direction;
+        private int wind_speed;
+        private enum Precipitation
+        { 
+        sunny,
+        cloudy,
+        rain,
+        snow
+        }
+        pubk
+    }
+}
